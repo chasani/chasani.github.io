284b90fa88bb9b780c032aa7f51798e4b3af168b
chasani <trevorshunte@gmail.com>
2025-08-15 18:51:01 -0500

    general update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
     // ...
     site: 'https://chasani.github.io',
-    base: 'https://chasani.github.io',
+    base: 'https://chasani.github.io/src',
     integrations: [tailwind({
         // nesting: true,
         applyBaseStyles: false,
