e433edefcabde6eb471e631e6b8fa2dfb7fa579d
chasani <trevorshunte@gmail.com>
2025-08-15 20:03:40 -0500

    general update

--- src/scripts/demo3.js
+++ src/scripts/demo3.js
@@ -242,23 +242,10 @@ document.addEventListener('DOMContentLoaded', () => {
         // title: fruit.replace('-', ' '),
         // description: document.querySelector(`#${fruit}`).querySelector('.description').innerHTML,
         // price: parseInt(document.querySelector(`#${fruit}`).getAttribute('data-price')),
-        amount: 0,
+        // amount: 0,
         in_cart: false
     }
 
-    const mango = {
-        _id: "mango",
-        vid: "",
-        thumb_1: `https://chasani.github.io/_astro/banana-thumb-1.BPrnxREf.png`,
-        thumb_2: `/src/assets/market/banana-thumb-2.png?origWidth=2048&origHeight=2048&origFormat=png`,
-        thumb_3: `/src/assets/market/banana-thumb-3.png?origWidth=2048&origHeight=2048&origFormat=png`,
-        // thumb_4: `/src/assets/market/${fruit}-thumb-4.png?origWidth=2048&origHeight=2048&origFormat=png`,
-        // title: fruit.replace('-', ' '),
-        // description: document.querySelector(`#${fruit}`).querySelector('.description').innerHTML,
-        // price: parseInt(document.querySelector(`#${fruit}`).getAttribute('data-price')),
-        amount: 0,
-        in_cart: false
-    }
 
 
     function createItem(fruit) {
